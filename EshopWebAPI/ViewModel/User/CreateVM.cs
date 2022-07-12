3edbf6bcea19664f6525b93929e80c5139b9fe2c
mihran-w <mihranw@gmail.com>
2022-07-12 17:40:45 +0430

    add view model and fix bug

---
+++ EshopWebAPI/ViewModel/User/CreateVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EshopWebAPI.ViewModel.User
+{
+    public class CreateVM
+    {
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "وارد کردن {} الزامی میباشد")]
+        public string Name { get; set; }
+        [Display(Name = "نام خانوادگی")]
+        [Required(ErrorMessage = "وارد کردن {} الزامی میباشد")]
+        public string Family { get; set; }
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "وارد کردن {} الزامی میباشد")]
+        public string UserName { get; set; }
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "وارد کردن {} الزامی میباشد")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
