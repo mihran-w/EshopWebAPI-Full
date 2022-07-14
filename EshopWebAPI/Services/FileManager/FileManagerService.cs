499377abbe52ccddca503342e90e04b8a8f41e7d
mihran-w <mihranw@gmail.com>
2022-07-15 01:18:58 +0430

    add file manager service

---
+++ EshopWebAPI/Services/FileManager/FileManagerService.cs
@@ -0,0 +1,24 @@
+﻿namespace EshopWebAPI.Services.FileManager
+{
+    public class FileManagerService : IFileManager
+    {
+        public string SaveFile(IFormFile file, string savePath)
+        {
+            if (file == null)
+                throw new Exception("مقدار ورودی فایل خالی است");
+
+            var filename = $"{Guid.NewGuid()}{file.FileName}";
+
+            var folderPath = Path.Combine(Directory.GetCurrentDirectory(), savePath.Replace("/","\\"));
+
+            if(!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            var fullPath = Path.Combine(folderPath, filename);
+            using var fs = new FileStream(fullPath, FileMode.Create);
+            file.CopyTo(fs);
+
+            return filename;
+        }
+    }
+}
---
+++ EshopWebAPI/Services/FileManager/IFileManager.cs
@@ -0,0 +1,7 @@
+﻿namespace EshopWebAPI.Services.FileManager
+{
+    public interface IFileManager
+    {
+        string SaveFile(IFormFile file, string savePath); 
+    }
+}
