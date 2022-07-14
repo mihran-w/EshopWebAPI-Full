6dc1019c1543f4ccdc799360574a1a834891f7f5
mihran-w <mihranw@gmail.com>
2022-07-14 17:53:45 +0430

    add sign in and product page

---
+++ EshopWeb/Pages/product.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EshopWeb.Pages
+{
+    public class Create_ProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
